a420f42f2081a6632d24a94fad190f5568cdd32b
Luc Bronsdijk <info@lucbronsdijk.nl>
2015-08-28 14:21:20 +0200

    Updatge javascript resoureces

--- src/js/app.js
+++ src/js/app.js
@@ -4,26 +4,25 @@ $(document).ready(function(){
 
     function initQuotes(){
         var quotesArray = [
-            'I do love <br/><a href="http://www.flickr.com/photos/lbronsdijk/sets/72157629787097051/" target="_blank">photography</a>',
             'I go by the<br/>name <strong>Luc</strong>',
+            'I push code <br/>to <a href="https://github.com/lbronsdijk" target="_blank" alt="Spotify">GitHub</a>',
+            'I do love <br/><a href="http://www.flickr.com/photos/lbronsdijk/sets/72157629787097051/" target="_blank">photography</a>',
             'I tweet on<br/><a href="https://twitter.com/lbronsdijk" target="_blank">Twitter</a>',
             'I am developer<br/> at <a href="http://www.angrybytes.nl/" target="_blank">Angry Bytes</a>',
-            'I drink a lots of <br/><strong>CocaCola</strong>',
             'I drop photos on <br/><a href="http://instagram.com/lbronsdijk" target="_blank" alt="Instagram">Instagram</a>',
             'I am good friends<br/>with <a href="http://www.apple.com/" target="_blank" alt="Apple">Apple</a>',
             'I drink<br/><a href="http://en.starbucks.nl/" target="_blank" alt="Starbucks">Starbucks</a> coffee',
             'I listen to the <br/>radiostation <a href="http://3fm.nl/" target="_blank" alt="3FM">3FM</a>',
-            'I stream music <br/>from <a href="http://open.spotify.com/user/lbronsdijk" target="_blank" alt="Spotify">Spotify</a>',
-            'I push code <br/>to <a href="https://github.com/lbronsdijk" target="_blank" alt="Spotify">GitHub</a>',
+            'I stream music <br/>from <a href="http://open.spotify.com/user/lbronsdijk" target="_blank" alt="Spotify">Spotify</a>'
         ];
         $('header').randomText( quotesArray, 5000 ); // ( array, interval, ["reload text or html"] )
     }
-})
+});
 
 $.fn.randomText = function( quotesArray, interval, prevText ) {
     var obj = $(this);
 
-    if ($('.quotes').length == 0) { 
+    if ($('.quotes').length === 0) { 
         obj.append('<h1 class="quotes animated bounceInDown">'); 
     }
 
@@ -46,10 +45,11 @@ $.fn.randomText = function( quotesArray, interval, prevText ) {
         }, 
         interval 
     );
-}
+};
 
 function random_array(aArray) {
     var rand = Math.floor( Math.random() * aArray.length + aArray.length );
     var randArray = aArray[ rand - aArray.length ];
+    
     return randArray;
 }
